611c994f6c506cbe0ae71daaabdca8fef7e6877d
tonyalcorn <xmastony@gmail.com>
2020-11-17 14:25:53 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // const input = document.querySelector('#new-task-description')
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const ul = document.querySelector('#tasks')
+    const li = document.createElement('li')
+    const text = document.createElement('text')
+    const input = document.querySelector('#new-task-description')
+    text.innerText = input.value
+    li.appendChild(text) 
+    ul.appendChild(li)
+  })
 });
